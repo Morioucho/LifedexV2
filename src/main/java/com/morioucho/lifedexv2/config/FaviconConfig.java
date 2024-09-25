6178f4f99fb9f232b1e67727523d6d5ca3bd3641
Galagyy <109639693+Galagyy@users.noreply.github.com>
2024-09-24 20:23:20 -0700

    Favicon Addition
    Added the Favicon configuration so that Favicons can load on the website.

---
+++ src/main/java/com/morioucho/lifedexv2/config/FaviconConfig.java
@@ -0,0 +1,31 @@
+package com.morioucho.lifedexv2.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import org.springframework.web.servlet.handler.SimpleUrlHandlerMapping;
+import org.springframework.web.servlet.resource.ResourceHttpRequestHandler;
+
+import java.util.Collections;
+
+@Configuration
+public class FaviconConfig {
+    @Bean
+    public SimpleUrlHandlerMapping customFaviconHandlerMapping() {
+        SimpleUrlHandlerMapping mapping = new SimpleUrlHandlerMapping();
+        mapping.setOrder(Integer.MIN_VALUE);
+        mapping.setUrlMap(Collections.singletonMap(
+                "/favicon.ico", faviconRequestHandler()));
+
+        return mapping;
+    }
+
+    @Bean
+    protected ResourceHttpRequestHandler faviconRequestHandler() {
+        ResourceHttpRequestHandler requestHandler
+                = new ResourceHttpRequestHandler();
+        requestHandler.setLocations(Collections.singletonList(new ClassPathResource("/")));
+        return requestHandler;
+    }
+}
