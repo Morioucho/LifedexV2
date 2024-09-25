77f8908afedef688859fec6a133433defcd46f9b
Saumil Sharma <109639693+Galagyy@users.noreply.github.com>
2024-09-25 09:32:10 -0700

    ViewStatistic Addition
    Addition of a ViewStatistic in order to have statistics for each page.

---
+++ src/main/java/com/morioucho/lifedexv2/model/ViewStatistic.java
@@ -0,0 +1,73 @@
+package com.morioucho.lifedexv2.model;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+public class ViewStatistic {
+    private long allTimeViews;
+    private long weeklyViews;
+    private long dailyViews;
+
+    private ZonedDateTime currentDateEnd;
+    private ZonedDateTime currentWeekEnd;
+
+    public ViewStatistic(){
+        ZoneId zoneId = ZoneId.of("America/New_York") ;
+        LocalDate today = LocalDate.now(zoneId) ;
+
+        this.currentDateEnd = today.plusDays(1).atStartOfDay(zoneId);
+        this.currentWeekEnd = today.plusDays(7).atStartOfDay(zoneId);
+    }
+
+    public void view(LocalDateTime time){
+        checkViewClears();
+
+        dailyViews++;
+        weeklyViews++;
+        allTimeViews++;
+    }
+
+    private void checkViewClears(){
+        if(ZonedDateTime.now().isAfter(currentWeekEnd)){
+            currentWeekEnd = currentWeekEnd.plusDays(7);
+            currentDateEnd = currentDateEnd.plusDays(1);
+
+            this.weeklyViews = 0;
+            this.dailyViews = 0;
+
+            return;
+        }
+
+        if(ZonedDateTime.now().isAfter(currentDateEnd)){
+            currentDateEnd = currentDateEnd.plusDays(1);
+
+            this.dailyViews = 0;
+        }
+    }
+
+    public long getAllTimeViews() {
+        return allTimeViews;
+    }
+
+    public void setAllTimeViews(long allTimeViews) {
+        this.allTimeViews = allTimeViews;
+    }
+
+    public long getWeeklyViews() {
+        return weeklyViews;
+    }
+
+    public void setWeeklyViews(long weeklyViews) {
+        this.weeklyViews = weeklyViews;
+    }
+
+    public long getDailyViews() {
+        return dailyViews;
+    }
+
+    public void setDailyViews(long dailyViews) {
+        this.dailyViews = dailyViews;
+    }
+}
