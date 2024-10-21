a2772f59d0c2618a60babf5d0b1e27778958361a
Kason Lai <kason_lai@yahoo.com>
2024-10-21 13:23:28 -0700

    Work on Recipes
    Work on the Recipe hub.

--- src/main/resources/static/js/articleparent.js
+++ src/main/resources/static/js/articleparent.js
@@ -1,9 +1,29 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const track = document.getElementById('track'); //Variable to carousel track.
+document.addEventListener("DOMContentLoaded", () => {
+    const track = document.getElementsByClassName('carousel__track'); //Variable to carousel track.
     const slides = Array.from(track.children);
-    const rightbutton = document.getElementById('rightbutton');
-    const leftbutton = document.getElementById('leftbutton');
-    document.addEventListener(onclick, { 
+    const rightButton = document.getElementsByClassName('carousel__button--right');
+    const leftButton = document.getElementsByClassName('carousel__button--left');
+    const dotNav = document.getElementsByClassName('carousel__nav');
+    const dot = Array.from(dotNav.children);
 
+    const slideWidth = slides[0].getBoundingClientRect().width;
+    // Width set.
+
+    //Slides need to be arranged.
+    // I need to set a slide array to move from. Make sure the CSS is set to overflow.
+
+    const slidePosition = (index) => {
+        for (var index = 0; index < slides.length; index++) {
+        slides[index].style.left = slideWidth * index + "px"
+    }};
+    //The slide buttons have to move the images around.
+    rightButton.addEventListener("click", e => {
+        const currentSlide = track.getElementsByClassName('current-Slide');
+        const nextSlide = currentSlide.nextElementSibling;
+        const moveDistance = nextSlide.style.left;
+
+        track.style.transform = 'translateX(-' + moveDistance + ')';
     })
+    // The dots have to change to match the slide index.
+    // The slide buttons have to be removed when the end of the index is hit.
 })
