eb9e71664ce70e92bc2bae97f3580d5f46972277
Kason Lai <kason_lai@yahoo.com>
2024-10-20 22:25:03 -0700

    Carousel Work
    Work on the carousel for posts and recipes.

---
+++ src/main/resources/static/js/articleparent.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const track = document.getElementById('track'); //Variable to carousel track.
+    const slides = Array.from(track.children);
+    const rightbutton = document.getElementById('rightbutton');
+    const leftbutton = document.getElementById('leftbutton');
+    document.addEventListener(onclick, { 
+
+    })
+})
