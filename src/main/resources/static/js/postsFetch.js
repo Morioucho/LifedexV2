e87b9793b52959405583e85872f686c65099ce22
Kason Lai <kason_lai@yahoo.com>
2024-10-22 10:23:14 -0700

    Modification and Renaming
    Modified postsandrecipes.js to become postsFetch.js and have the code specifically for fetching information for posts.

---
+++ src/main/resources/static/js/postsFetch.js
@@ -0,0 +1,82 @@
+document.addEventListener("DOMContentLoaded", function () {
+  class Article {
+    constructor(
+      title,
+      image,
+      description,
+      author,
+      date
+    ) {
+      this.title = title;
+      this.image = image;
+      this.description = description;
+      this.author = author;
+      this.date = date;
+    }
+
+    getTitle() {
+      return this.title;
+    }
+    setTitle(title) {
+      this.title = title;
+    }
+
+    getImage() {
+      return this.image;
+    }
+    setImage(image) {
+      this.image = image;
+    }
+
+    getDescription() {
+      return this.description;
+    }
+    setDescription(description) {
+      this.description = description;
+    }
+
+    getAuthor() {
+      return this.author;
+    }
+    setAuthor(author) {
+      this.author = author;
+    }
+
+    getDate() {
+      return this.date;
+    }
+    setDate(date) {
+      this.date = date;
+    }
+  }
+
+  function renderArticleInfo(article, elementId) {
+    const articleInfoHtml = `
+      ${article.title}<br>
+      ${article.image}<br>
+      ${article.description}<br>
+      ${article.author}<br>
+      ${article.date}<br>
+    `;
+    
+    document.getElementById(elementId).innerHTML = articleInfoHtml;
+  }
+
+  async function articleDataFetch() {
+    try {
+      const response = await fetch("/api/posts", {
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
+      });
+      const articleInfo = await response.json();
+
+      for (var i = 0; i < articleInfo.length; i++) {
+        renderArticleInfo(articleInfo[i]);
+      }
+
+    } catch (error) {
+      console.error("Error fetching article data:", error);
+    }
+  }
+  articleDataFetch();
+});
