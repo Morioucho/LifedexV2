43ea30dbf1ddb46b92cd1c04672e351bc62e947f
Kason Lai <kason_lai@yahoo.com>
2024-10-21 22:51:05 -0700

    Updated a File for Future Use
    Sourced code from a previous work that will be applied for a similar use.

--- src/main/resources/static/js/postsandrecipes.js
+++ src/main/resources/static/js/postsandrecipes.js
@@ -1,29 +1,119 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const track = document.querySelector('.carousel__track'); //Variable to carousel track.
-    const slides = Array.from(track.children);
-    const rightButton = document.querySelector('.carousel__button--right');
-    const leftButton = document.querySelector('.carousel__button--left');
-    const dotNav = document.querySelector('.carousel__nav');
-    const dot = Array.from(dotNav.children);
+document.addEventListener("DOMContentLoaded", function () {
+  class Planet {
+    constructor(
+      name,
+      mass,
+      radius,
+      dayLength,
+      yearLength,
+      eccentricity,
+      axialTilt,
+      perihelion,
+      aphelion
+    ) {
+      this.name = name;
+      this.mass = mass;
+      this.radius = radius;
+      this.dayLength = dayLength;
+      this.yearLength = yearLength;
+      this.eccentricity = eccentricity;
+      this.axialTilt = axialTilt;
+      this.perihelion = perihelion;
+      this.aphelion = aphelion;
+    }
 
-    const slideWidth = slides[0].getBoundingClientRect().width;
-    // Width set.
+    getName() {
+      return this.name;
+    }
+    setName(name) {
+      this.name = name;
+    }
 
-    //Slides need to be arranged.
-    // I need to set a slide array to move from. Make sure the CSS is set to overflow.
+    getRadius() {
+      return this.radius;
+    }
+    setRadius(radius) {
+      this.radius = radius;
+    }
 
-    const slidePosition = (index) => {
-        for (var index = 0; index < slides.length; index++) {
-        slides[index].style.left = slideWidth * index + "px"
-    }};
-    //The slide buttons have to move the images around.
-    rightButton.addEventListener("click", e => {
-        const currentSlide = track.querySelector('.current-Slide');
-        const nextSlide = currentSlide.nextElementSibling;
-        const moveDistance = nextSlide.style.left;
-        console.log(moveDistance);
-        track.style.transform = 'translateX(-' + moveDistance + ')';
-    })
-    // The dots have to change to match the slide index.
-    // The slide buttons have to be removed when the end of the index is hit.
-})
+    getDayLength() {
+      return this.dayLength;
+    }
+    setDayLength(dayLength) {
+      this.dayLength = dayLength;
+    }
+
+    getYearLength() {
+      return this.yearLength;
+    }
+    setYearLength(yearLength) {
+      this.yearLength = yearLength;
+    }
+
+    getEccentricity() {
+      return this.eccentricity;
+    }
+    setEccentricity(eccentricity) {
+      this.eccentricity = eccentricity;
+    }
+
+    getAxialTilt() {
+      return this.axialTilt;
+    }
+    setAxialTilt(axialTilt) {
+      this.axialTilt = axialTilt;
+    }
+
+    getPerihelion() {
+      return this.perihelion;
+    }
+    setPerihelion(perihelion) {
+      this.perihelion = perihelion;
+    }
+
+    getAphelion() {
+      return this.aphelion;
+    }
+    setAphelion(aphelion) {
+      this.aphelion = aphelion;
+    }
+  }
+
+  function renderPlanetInfo(planet, elementId) {
+    const planetInfoHtml = `
+      ${planet.name}<br>
+      ${planet.mass.toExponential().replace(/e\+?/, " x 10^")}<br>
+      ${planet.radius}<br>
+      ${planet.dayLength.toExponential().replace(/e\+?/, " x 10^")}<br>
+      ${planet.yearLength.toExponential().replace(/e\+?/, " x 10^")}<br>
+      ${planet.eccentricity}<br>
+      ${planet.axialTilt}<br>
+      ${planet.perihelion.toExponential().replace(/e\+?/, " x 10^")}<br>
+      ${planet.aphelion.toExponential().replace(/e\+?/, " x 10^")}
+    `;
+    
+    document.getElementById(elementId).innerHTML = planetInfoHtml;
+  }
+
+  async function planetDataFetch() {
+    try {
+      const response = await fetch("/api/planets", {
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
+      });
+      const planetInfo = await response.json();
+
+      renderPlanetInfo(planetInfo[0], "mercury-planet-information");
+      renderPlanetInfo(planetInfo[1], "venus-planet-information");
+      renderPlanetInfo(planetInfo[2], "earth-planet-information");
+      renderPlanetInfo(planetInfo[3], "mars-planet-information");
+      renderPlanetInfo(planetInfo[4], "jupiter-planet-information");
+      renderPlanetInfo(planetInfo[5], "saturn-planet-information");
+      renderPlanetInfo(planetInfo[6], "uranus-planet-information");
+      renderPlanetInfo(planetInfo[7], "neptune-planet-information");
+    } catch (error) {
+      console.error("Error fetching planet data:", error);
+    }
+  }
+  planetDataFetch();
+});
